e5a232c15dbc4151d4f000956d70bb745dc015df
ami_ba <amitayasb@gmail.com>
2021-09-24 12:02:14 +0530

    pushing fish config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+set fish_greeting
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export EDITOR=nvim
+
+#colorscript random
+
+
+neofetch
+starship init fish | source
