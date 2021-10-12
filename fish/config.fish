0bbf2c300eebc8cc32f7f1f98eafdc037f800632
ami_ba <amitayasb@gmail.com>
2021-10-13 00:27:54 +0530

    multiple changes

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,10 @@ end
 export EDITOR=nvim
 export SHELL=fish
 
-#colorscript random
-
-
-neofetch
-starship init fish | source
+colorscript random
+#neofetch
+#neofetch --kitty  ~/Pictures/arch-linux.png
+eval (starship init fish)
 
 #neofetch
 #fortune | ponysay
